8d064d357f44081d2ef5878fe3f29c96fe4d8abf
morales04 <andres.4moscoso@gmail.com>
2026-03-29 13:14:05 -0500

    cambios en detalle libro y lista libros

--- catalogo_libros/urls.py
+++ catalogo_libros/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 # Se crea un archivo urls.py dentro de la app para definir las rutas especificas de la app,y luego se incluyen estas rutas en el archivo urls.py del proyecto
 
 urlpatterns = [
-    path("", views.lista_libros, name="lista_libros"),
+    path("", views.lista_libros, name="libros"),
     path("libro/<int:id>/", views.detalle_libro, name="detalle_libro"),
 ]
